d44ecdd02bf16460bc10bf7f84105d599a685e7b
Paul Caillier <paul.caillier2002@gmail.com>
2023-01-09 21:32:13 +0100

    Init project

---
+++ scripts/get_tortoises_png.js
@@ -0,0 +1,27 @@
+// Read emoji data from a JSON file and download all of the turtles pictures
+const fs = require("fs")
+const client = require('https');
+
+emojiJson = fs.readFileSync("./emojiOutput.json");
+emojiData = JSON.parse(emojiJson);
+
+(async ()=>{
+    count = 0;
+    for (turtle of emojiData["1f422"]) {
+        turtle.leftEmoji = turtle.leftEmoji.split("-").reduce((acc, curr) => acc + "-u" + curr, "").slice(1)
+        turtle.rightEmoji = turtle.rightEmoji.split("-").reduce((acc, curr) => acc + "-u" + curr, "").slice(1)
+        console.log(`${turtle.leftEmoji}_${turtle.rightEmoji}.png`);
+        await downloadTurtle(turtle, count)
+        count++;
+    }
+})();
+
+async function downloadTurtle(turtle, name) {
+    return new Promise((resolve) => {
+        url = `https://www.gstatic.com/android/keyboard/emojikitchen/${turtle.date}/${turtle.leftEmoji}/${turtle.leftEmoji}_${turtle.rightEmoji}.png`
+        client.get(url, (res) => {
+            res.pipe(fs.createWriteStream(`./tortoises_data/tortoises_png/${name}.png`));
+        });
+        resolve();
+    })
+}
