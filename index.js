bc55e5824ba49fd611ee82513bda65c4b5312a27
Paul Caillier <paul.caillier2002@gmail.com>
2023-01-10 22:09:15 +0100

    Add random turtle command

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 require('dotenv').config()
 const discord = require('discord.js');
 
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
+function randInt(min, max) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
+
+const commandsArr = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+    {
+        name: 'tortoise',
+        description: 'Displays a random tortoise'
+    }
 ];
 
-const rest = new discord.REST().setToken(process.env.TOKEN);
-const client = new discord.Client({intents: [discord.GatewayIntentBits.Guilds]});
+commands = new discord.Collection();
+
+for (command of commandsArr) {
+    console.log(command);
+    commands.set(command.name, command);
+}
+
+const rest = new discord.REST({ version: '10' }).setToken(process.env.TOKEN);
+const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
 
 (async () => {
     try {
-      console.log('Updating / applications...');
-      await rest.put(discord.Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
-      console.log('Updated !');
+        console.log('Updating / applications...');
+        await rest.put(discord.Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+        console.log('Updated !');
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
 })();
 
 client.on('interactionCreate', async (interaction) => {
-    if(!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === 'ping') {
         await interaction.reply('Pong!');
+    }
+
+    if (interaction.commandName === 'tortoise') {
+        await interaction.reply({ content: "Here you go !", files: [`./tortoises_data/tortoises_png/${randInt(0,217)}.png`]});
     }
 })
 client.login(process.env.TOKEN);
